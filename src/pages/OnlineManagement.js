ae4e6d9b963ae54501ac6cd703b1c4e2e21ad3aa
sonukushwah123 <kushwa378@gmail.com>
2023-02-12 17:45:57 +0530

    loader management

--- src/pages/OnlineManagement.js
+++ src/pages/OnlineManagement.js
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import env from "react-dotenv";
+import Loader from "./Loader";
 let urlApi = `${process.env.REACT_APP_APIURL}`;
 
 console.log(`${process.env.REACT_APP_APIURL}`);
@@ -14,8 +15,10 @@ const OnlineManagement = () => {
   console.log(id, "id");
 
   const [post, SetPost] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     const tokenID = localStorage.getItem("token");
     console.log("id", id);
     const fetch = async () => {
@@ -30,6 +33,7 @@ const OnlineManagement = () => {
           }
         );
         SetPost(data);
+        setIsLoading(false);
       } catch (err) {
         console.error(err);
       }
@@ -45,94 +49,99 @@ const OnlineManagement = () => {
 
   return (
     <div>
-      <section className="blog_sec">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-9">
-              <div className="term_sec-9">
-                <img
-                  src={urlApi + "/image/" + post?.data?.image}
-                  className="blog_sec-img"
-                />
-                <div className="p-4">
-                  <h2 className="blog_sec-h2 p-0">{post?.data?.title} </h2>
-                  <div className="row">
-                    <span className=" col-md-6 blog-span1  m-0">
-                      {post?.data?.name}{" "}
-                    </span>
-                    <span className=" col-md-6 text-end  blog-span2 m-0">
-                      September 08,2022
-                    </span>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <section className="blog_sec">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-9">
+                <div className="term_sec-9">
+                  <img
+                    src={urlApi + "/image/" + post?.data?.image}
+                    className="blog_sec-img"
+                  />
+                  <div className="p-4">
+                    <h2 className="blog_sec-h2 p-0">{post?.data?.title} </h2>
+                    <div className="row">
+                      <span className=" col-md-6 blog-span1  m-0">
+                        {post?.data?.name}{" "}
+                      </span>
+                      <span className=" col-md-6 text-end  blog-span2 m-0">
+                        September 08,2022
+                      </span>
+                    </div>
+                    <RenderHTML HTML={post?.data?.dec} />
                   </div>
-                  <RenderHTML HTML={post?.data?.dec} />
+                </div>
+              </div>
+
+              <div className="col-md-3 term_sec-3">
+                <div className="block1">
+                  <form>
+                    <p className="block_p">Search</p>
+                    <input type="text" name="search" className="block_search" />
+                    <button type="submit" className="block_btn">
+                      search
+                    </button>
+                  </form>
+                </div>
+
+                <div className="block1">
+                  <h3 className="block-h3">Recent Posts</h3>
+                  <p className="block-p2">
+                    <Link to="" className="block_a">
+                      Online Reputation Management
+                    </Link>
+                  </p>
+                  <p className="block-p2">
+                    <Link to="" className="block_a">
+                      Marketing research case analysis of Google search engine
+                    </Link>
+                  </p>
+                  <p className="block-p2">
+                    <Link to="" className="block_a">
+                      A 5-Step Guide to Protecting Your Health and Well-Being
+                      While Working from Home
+                    </Link>
+                  </p>
+
+                  <p className="block-p2">
+                    <Link to="" className="block_a">
+                      The Art of Content Writing
+                    </Link>
+                  </p>
+                  <p className="block-p2">
+                    <Link to="" className="block_a">
+                      Tips to write better content that gets you up in the
+                      night.
+                    </Link>
+                  </p>
+                </div>
+
+                <div className="block1">
+                  <h3 className="block-h3">Recent Comments</h3>
+                  <p className="block-p3">No comments to show.</p>
+                </div>
+
+                <div className="block1">
+                  <h3 className="block-h3">Archives</h3>
+                  <p className="block-p3">February 2022</p>
+                  <p className="block-p3">January 2022</p>
+                  <p className="block-p3">December 2021</p>
+                </div>
+
+                <div className="block1">
+                  <h3 className="block-h3">Categories</h3>
+                  <p className="block-p3">Content Writing</p>
+                  <p className="block-p3">General</p>
+                  <p className="block-p3">Tips</p>
                 </div>
               </div>
             </div>
-
-            <div className="col-md-3 term_sec-3">
-              <div className="block1">
-                <form>
-                  <p className="block_p">Search</p>
-                  <input type="text" name="search" className="block_search" />
-                  <button type="submit" className="block_btn">
-                    search
-                  </button>
-                </form>
-              </div>
-
-              <div className="block1">
-                <h3 className="block-h3">Recent Posts</h3>
-                <p className="block-p2">
-                  <Link to="" className="block_a">
-                    Online Reputation Management
-                  </Link>
-                </p>
-                <p className="block-p2">
-                  <Link to="" className="block_a">
-                    Marketing research case analysis of Google search engine
-                  </Link>
-                </p>
-                <p className="block-p2">
-                  <Link to="" className="block_a">
-                    A 5-Step Guide to Protecting Your Health and Well-Being
-                    While Working from Home
-                  </Link>
-                </p>
-
-                <p className="block-p2">
-                  <Link to="" className="block_a">
-                    The Art of Content Writing
-                  </Link>
-                </p>
-                <p className="block-p2">
-                  <Link to="" className="block_a">
-                    Tips to write better content that gets you up in the night.
-                  </Link>
-                </p>
-              </div>
-
-              <div className="block1">
-                <h3 className="block-h3">Recent Comments</h3>
-                <p className="block-p3">No comments to show.</p>
-              </div>
-
-              <div className="block1">
-                <h3 className="block-h3">Archives</h3>
-                <p className="block-p3">February 2022</p>
-                <p className="block-p3">January 2022</p>
-                <p className="block-p3">December 2021</p>
-              </div>
-
-              <div className="block1">
-                <h3 className="block-h3">Categories</h3>
-                <p className="block-p3">Content Writing</p>
-                <p className="block-p3">General</p>
-                <p className="block-p3">Tips</p>
-              </div>
-            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
     </div>
   );
 };
--- src/pages/ViewDetails.js
+++ src/pages/ViewDetails.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import env from "react-dotenv";
+import Loader from "./Loader";
 
 const ViewDetails = () => {
   let urlApi = `${process.env.REACT_APP_APIURL}`;
@@ -11,7 +12,9 @@ const ViewDetails = () => {
   console.log(id, "id");
 
   const [posts, SetPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
+    setIsLoading(true);
     console.log("id", id);
     const fetch = async () => {
       try {
@@ -19,6 +22,7 @@ const ViewDetails = () => {
           `${process.env.REACT_APP_APIURL}/getAuthor/${id}`
         );
         SetPosts(data);
+        setIsLoading(false);
       } catch (err) {
         console.error(err);
       }
@@ -33,40 +37,46 @@ const ViewDetails = () => {
   console.log(posts);
 
   return (
-    <section className="team_sec">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <div
-              className="author_box p-3 text-center"
-              style={{ border: "1px solid #029A99" }}
-            >
-              <img
-                src={urlApi + "/image/" + posts?.data?.image}
-                alt="author-img"
-                className="author_sec-img ms-0"
-                style={{ border: "5px solid #029A99" }}
-              />
-              <h4 className="author_Sec-h2 fs-2">{posts?.data?.title}</h4>
-              <br />
-              <p className="author_Sec-p fs-4">
-                Designation:{" "}
-                <span className="text-dark">{posts?.data?.dec}</span>
-              </p>
-              <RenderHTML HTML={posts?.data?.longDec} />{" "}
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <section className="team_sec">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4">
+                <div
+                  className="author_box p-3 text-center"
+                  style={{ border: "1px solid #029A99" }}
+                >
+                  <img
+                    src={urlApi + "/image/" + posts?.data?.image}
+                    alt="author-img"
+                    className="author_sec-img ms-0"
+                    style={{ border: "5px solid #029A99" }}
+                  />
+                  <h4 className="author_Sec-h2 fs-2">{posts?.data?.title}</h4>
+                  <br />
+                  <p className="author_Sec-p fs-4">
+                    Designation:{" "}
+                    <span className="text-dark">{posts?.data?.dec}</span>
+                  </p>
+                  <RenderHTML HTML={posts?.data?.longDec} />{" "}
+                </div>
+              </div>
+              <div className="col-md-8">
+                <h2 className="author_Sec-h2 article  text-start ">
+                  Articles by {posts?.data?.title}
+                </h2>
+                <p className="author_Sec-p text-start fs-6 mt-4">
+                  Sorry there is no article found for this author....!
+                </p>
+              </div>
             </div>
           </div>
-          <div className="col-md-8">
-            <h2 className="author_Sec-h2 article  text-start ">
-              Articles by {posts?.data?.title}
-            </h2>
-            <p className="author_Sec-p text-start fs-6 mt-4">
-              Sorry there is no article found for this author....!
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
+        </section>
+      )}
+    </>
   );
 };
 
