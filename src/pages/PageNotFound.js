9873120ac69c29d66e22ed869ea8575b4d3c3296
Nian01 <niantechnologies@gmail.com>
2023-05-10 12:02:57 +0000

    Updated frontend code

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -2,23 +2,19 @@ import React from "react";
 
 const Prices = () => {
 return(
-    <div className="container">
-        <div className="row">
-         <div className="text-center sm-12">
-           <img src="./writer/img/nicubunu_Stickman_09.png" style={{width:"10%"}}/>
-            <h4 className="mt-3" style={{color:"#2ab8d3"}}>Hmm, </h4>
-                   <h4  style={{color:"#2ab8d3"}}>This page dose not exist or has moved on the somthing else </h4>
-                 <p className="mt-5">Try one of the link in the header to reach the designation page or link below</p>
+<div className="text-center">
+    <img src="./writer/img/nicubunu_Stickman_09.png" style={{width:"10%"}}/>
+    <h4 className="mt-3" style={{color:"#2ab8d3"}}>Hmm, </h4>
+    <h4  style={{color:"#2ab8d3"}}>This page dose not exist or has moved on the somthing else </h4>
+    <p className="mt-5">Try one of the link in the header to reach the designation page or link below</p>
 
-          <div className="text-4 mt-2 sm-12 mb-4 ">
-                <button type="button" className="btn" style={{background:"#2ab8d3"}}><a style={{color:"white", textDecoration:"none"}} href="https://getprowriter.com/">Home</a></button> &nbsp;&nbsp;&nbsp;&nbsp;
-                 <button type="button" className="btn" style={{background:"#2ab8d3"}}><a style={{color:"white", textDecoration:"none"}} href="https://getprowriter.com/guestpayment">MAKE PAYMENT</a></button>&nbsp;&nbsp;&nbsp;&nbsp;
-                  <button type="button" className="btn" style={{background:"#2ab8d3"}}><a style={{color:"white", textDecoration:"none"}} href="https://getprowriter.com/how-it-works">HOW IT WORKS</a></button>&nbsp;&nbsp;&nbsp;&nbsp;
-                     <button type="button" className="btn" style={{background:"#2ab8d3"}}><a style={{color:"white", textDecoration:"none"}} href="https://getprowriter.com/place-your-order">ORDER AN ASSIGNMENT</a></button>
-            </div>
-         </div>
-      </div>
-      </div>
+    <div className="d-flex justify-content-center mt-4">
+        <button className="btn" style={{background:"#2ab8d3"}}>Home</button> &nbsp;&nbsp;&nbsp;&nbsp;
+        <button className="btn" style={{background:"#2ab8d3"}}>MAKE PAYMENT</button>&nbsp;&nbsp;&nbsp;&nbsp;
+        <button className="btn" style={{background:"#2ab8d3"}}>HOE IT WORKS</button>&nbsp;&nbsp;&nbsp;&nbsp;
+        <button className="btn" style={{background:"#2ab8d3"}}>ORDER AN ASSIGNMENT</button>
+    </div>
+</div>
     );
 };
 
